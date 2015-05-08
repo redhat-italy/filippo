6a898e547376a11f2bafd2107a9736667a1664cf
ugol <ugol@redhat.com>
2015-05-08 12:44:05 +0200

    Added audio files

--- src/main/java/it/redhat/jokes/Filippo.java
+++ src/main/java/it/redhat/jokes/Filippo.java
@@ -1,10 +1,16 @@
 package it.redhat.jokes;
 
+import sun.audio.AudioPlayer;
+import sun.audio.AudioStream;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Random;
 
 public class Filippo {
 
-    public static void main( String[] args ) {
+    public static void main(String[] args) {
 
         Filippo pippo = new Filippo();
         System.out.println(pippo.says());
@@ -12,9 +18,21 @@ public class Filippo {
     }
 
     public String says() {
-         return sentences[new Random().nextInt(sentences.length)];
+        int index = new Random().nextInt(sentences.length);
+        InputStream in;
+        AudioStream audioStream = null;
+
+        try {
+            in = new FileInputStream(index + ".wav");
+            audioStream = new AudioStream(in);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        AudioPlayer.player.start(audioStream);
+        return sentences[index];
     }
 
-    private String[] sentences = {"Osu!", "Calmi...", "Calmo...", "Spacco tutto!", "Ma perche'?", "Che brutte persone...", "Perche'?"};
+    private String[] sentences = {"Osu!", "Calmi...", "Stai calmo...", "Spacco tutto!", "Ma perche'?", "Che brutte persone...", "Perche'?", "Che squallore...", "Mio fido allievo...", "Mammia mia Marinelli...", "Assumete Tarocchi!", "Mamma mia"};
 
 }
